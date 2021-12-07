24a54e277543278c2d93ea6ee7e03ecfe000eca5
yernar707 <ernarergaziev00@gmail.com>
2021-12-08 01:50:19 +0600

    FrontEnd Base

---
+++ src/main/resources/templates/footer.php
@@ -0,0 +1,20 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" style="height: 1.5px; background: #FFF;margin: auto;">
+				
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<img class="footer-logo" src="images/logo/logotext.png">
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" style="height: 1.5px; background: #FFF;margin: auto;">
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p>© All Rights Reserved. <br> By Team 11. Nazarbayev University. 2021</p>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ src/main/resources/templates/header.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navbar-light justify-content-center" id="navigation">
+	<div class="col-lg-1 navbar-nav-logo">
+		<!-- <a href="">
+			<img class="text-logo" src="images/logo/logotext.png">
+		</a> -->
+	</div>
+	<div class="col-lg-11 navbar-nav-center">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="#carousel">Main</a>
+			</li>
+			<li class="nav-item">
+				<div class="dropdown">
+					<a class="nav-link dropdown-toggle"  data-bs-toggle="dropdown">Find a Hotel</a>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="#">Golden Rose Antalya</a></li>
+						<li><a class="dropdown-item" href="#">Golden Rose Dubai</a></li>
+						<li><a class="dropdown-item" href="#">Golden Rose Nur-Sultan</a></li>
+						<li><a class="dropdown-item" href="#">Golden Rose Paris</a></li>
+						<li><a class="dropdown-item" href="#">Golden Rose California</a></li>
+					</ul>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a href="">
+					<img class="square-logo" src="images/logo/logo.png">
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#about">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#contact">Contact Us</a>
+			</li>
+		</ul>
+	</div>
+	<div class="col-lg-1 navbar-nav-right">
+		<a class="nav-link login" href="">
+			Make a reservation
+		</a>
+	</div>
+</nav>
