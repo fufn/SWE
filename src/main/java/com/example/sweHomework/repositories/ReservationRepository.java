28c5c9c473312874012a516fa6fc4ec8f13aa9a9
fufn <xfufnx@gmail.com>
2021-12-08 22:05:49 +0600

    RESERVATION AND USERS MODIFIED

--- src/main/java/com/example/sweHomework/repositories/ReservationRepository.java
+++ src/main/java/com/example/sweHomework/repositories/ReservationRepository.java
@@ -1,6 +1,5 @@
 package com.example.sweHomework.repositories;
 
-import com.example.sweHomework.entities.Hotels;
 import com.example.sweHomework.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
