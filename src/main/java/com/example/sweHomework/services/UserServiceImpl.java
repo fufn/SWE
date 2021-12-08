7326cfc27ac4defb1b274778f95db5a987a83bb3
fufn <xfufnx@gmail.com>
2021-12-08 22:54:27 +0600

    REGISTRATION ADDED

--- src/main/java/com/example/sweHomework/services/UserServiceImpl.java
+++ src/main/java/com/example/sweHomework/services/UserServiceImpl.java
@@ -1,24 +1,33 @@
 package com.example.sweHomework.services;
 
+import com.example.sweHomework.entities.Roles;
 import com.example.sweHomework.entities.Users;
+import com.example.sweHomework.repositories.RoleRepository;
 import com.example.sweHomework.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class UserServiceImpl implements UserService{
+import java.util.ArrayList;
+
+@Service
+public class UserServiceImpl implements UserService {
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
         Users user = userRepository.findByEmail(s);
 
-        if (user!=null){
+        if(user!=null){
             return user;
-        } else {
+        }else{
             throw new UsernameNotFoundException("USER NOT FOUND");
         }
 
@@ -26,11 +35,26 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public Users getUser(String email) {
-        return null;
+        return userRepository.findByEmail(email);
     }
 
     @Override
     public Users addUser(Users user) {
+
+        Users checkUser = userRepository.findByEmail(user.getEmail());
+        if(checkUser==null){
+            Roles userRole = roleRepository.findByRole("ROLE_GUEST");
+            ArrayList<Roles> roles = new ArrayList<>();
+            roles.add(userRole);
+            user.setRoles(roles);
+            return userRepository.save(user);
+        }
+
         return null;
+    }
+
+    @Override
+    public Users updateUser(Users user) {
+        return userRepository.save(user);
     }
 }
